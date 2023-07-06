7dd5d35f08fec9d00f4617524c097c09e957cb08
Andrii Khoma <anik123wb@gmail.com>
2023-07-06 15:10:22 +0300

    add public category tree dto

--- dto/category/categories-tree.dto.ts
+++ dto/category/categories-tree.dto.ts
@@ -1,10 +1,24 @@
 import { TranslatedText } from '../translated-text';
 
-export interface CategoriesNodeDto {
+export interface CategoriesNodeAdminDto {
   id: string;
   publicId: string;
   title: TranslatedText;
   description: TranslatedText;
+  children: CategoriesNodeAdminDto[];
+  sort: number;
+  active: boolean;
+}
+
+export interface CategoriesTreeAdminDto {
+  root: CategoriesNodeAdminDto[];
+}
+
+export interface CategoriesNodeDto {
+  id: string;
+  publicId: string;
+  title: string;
+  description: string;
   children: CategoriesNodeDto[];
   sort: number;
   active: boolean;
