41c12a8bf9a67a1fcb2aa422615ba978c342ba2b
Andrii Khoma <anik123wb@gmail.com>
2023-07-15 11:36:06 +0300

    many changes

--- dto/product/product-list.response.dto.ts
+++ dto/product/product-list.response.dto.ts
@@ -1,10 +1,11 @@
 import { ProductAttributesDto, ProductDto } from './product.dto';
+import {MoneySmall} from "@/shop-shared/dto/primitiveTypes";
 
 export class ProductListResponseDto {
   products!: ProductDto[];
   total!: number;
   filters!: ProductAttributesDto;
   categories: string[] = [];
-  priceMin!: number;
-  priceMax!: number;
+  priceMin!: MoneySmall;
+  priceMax!: MoneySmall;
 }
