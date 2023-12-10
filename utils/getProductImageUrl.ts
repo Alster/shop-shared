cb23dc2fcf75b0442b1a3dc510970b55dfa0c9c8
anikkhoma <anik123wb@gmail.com>
2023-12-10 17:02:07 +0200

    :hammer: updates

--- utils/getProductImageUrl.ts
+++ utils/getProductImageUrl.ts
@@ -1,11 +1,20 @@
+import getProductImageFilename from "./getProductImageFilename";
 import ImagePostfixType from "./imagePostfixType";
 
-export default function getProductImageUrl(
-	productId: string,
-	imageId: string,
-	postfix: ImagePostfixType,
-): string {
-	const kind = "product-images";
-	const filename = `${kind}/${productId}/${imageId}_${postfix}.jpeg`;
-	return `https://unicorn-bleak.s3.eu-central-1.amazonaws.com/${filename}`;
+const imagesDomainKey = "NEXT_PUBLIC_IMAGES_DOMAIN";
+
+const imagesDomain = process.env[imagesDomainKey] as string;
+
+if (!imagesDomain) {
+	throw new Error(`Environment variable ${imagesDomainKey} is not set`);
+}
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default function getProductImageUrl<
+	const ProductId extends string,
+	const ImageId extends string,
+	const Postfix extends ImagePostfixType,
+>(productId: ProductId, imageId: ImageId, postfix: Postfix) {
+	const filename = getProductImageFilename(productId, imageId, postfix);
+	return `${imagesDomain}/${filename}` as const;
 }
