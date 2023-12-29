820316afedd008adaf60dcaab7ca96c1d0f6aab4
anikkhoma <anik123wb@gmail.com>
2023-12-30 01:04:04 +0200

    :arrow_up: update

--- utils/getProductImageUrl.ts
+++ utils/getProductImageUrl.ts
@@ -1,11 +1,9 @@
 import ImagePostfixType from "./imagePostfixType";
 
-const imagesDomainKey = "NEXT_PUBLIC_IMAGES_DOMAIN";
-
-const imagesDomain = process.env[imagesDomainKey] as string;
+const imagesDomain = process.env.NEXT_PUBLIC_IMAGES_DOMAIN as string;
 
 if (!imagesDomain) {
-	throw new Error(`Environment variable ${imagesDomainKey} is not set`);
+	throw new Error(`Environment variable for imagesDomain is not set`);
 }
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
