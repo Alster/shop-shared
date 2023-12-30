cce6a5bac3d659329e9cde5a1bf16f1898708edc
anikkhoma <anik123wb@gmail.com>
2023-12-30 02:34:07 +0200

    :hammer: updates

--- utils/getProductImageUrl.ts
+++ utils/getProductImageUrl.ts
@@ -1,6 +1,6 @@
 import ImagePostfixType from "./imagePostfixType";
 
-const imagesDomain = process.env.NEXT_PUBLIC_IMAGES_DOMAIN as string;
+const imagesDomain = process.env["NEXT_PUBLIC_IMAGES_DOMAIN"] as string;
 
 if (!imagesDomain) {
 	throw new Error(`Environment variable for imagesDomain is not set`);
