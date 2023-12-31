f01ed520adb7a965050041385a14ea8a3dbf540a
anikkhoma <anik123wb@gmail.com>
2023-12-31 13:47:53 +0200

    :sparkles: updates

---
+++ utils/colorSorter.ts
@@ -0,0 +1,63 @@
+// @ts-expect-error - no typings for this package
+import { fromString } from "css-color-converter";
+
+interface IHSLA {
+	hue: number;
+	saturation: number;
+	lightness: number;
+	alpha: number;
+	reference: string;
+}
+
+const convert = (color: string): IHSLA => {
+	const [hue, saturation, lightness, alpha] = fromString(color).toHslaArray();
+
+	return {
+		hue,
+		saturation,
+		lightness,
+		alpha,
+		reference: color,
+	};
+};
+
+const compareColors = (a: string, b: string): number => {
+	const colorA = convert(a);
+	const colorB = convert(b);
+
+	// Move grey-ish values to the back
+	if (
+		(colorA.saturation === 0 || colorB.saturation === 0) &&
+		colorA.saturation !== colorB.saturation
+	) {
+		return colorB.saturation - colorA.saturation;
+	}
+
+	// Sort by hue (lowest first)
+	if (colorA.hue !== colorB.hue) {
+		return colorA.hue - colorB.hue;
+	}
+
+	// Sort by saturation (highest first)
+	if (colorA.saturation !== colorB.saturation) {
+		return colorA.saturation - colorB.saturation;
+	}
+
+	// Comparing gray values, light before dark
+	if (
+		colorA.saturation === 0 &&
+		colorB.saturation === 0 &&
+		colorA.lightness !== colorB.lightness
+	) {
+		return colorB.lightness - colorA.lightness;
+	}
+
+	// Sort by transparency, least transparent first
+	if (colorA.alpha === colorB.alpha) {
+		return colorA.reference.toLowerCase().localeCompare(colorB.reference.toLowerCase());
+	}
+
+	return colorB.alpha - colorA.alpha;
+};
+
+export default compareColors;
